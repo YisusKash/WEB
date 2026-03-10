818d80642d996706f0d9c2d0d83d05a50651693a
YisusKash <y.jalbin.1705@gmail.com>
2026-03-10 18:43:04 -0500

    version final

--- form.php
+++ form.php
@@ -1,26 +1,34 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$nombre = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$mensaje_usuario = htmlspecialchars($_POST['message']);
 
-$para = "nobody281812@gmail.com";
-$asunto = "Mensaje desde la página web";
+    $nombre = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $mensaje_usuario = htmlspecialchars($_POST['message']);
 
-$mensaje = "Nombre: $nombre\n";
-$mensaje .= "Email: $email\n\n";
-$mensaje .= "Mensaje:\n$mensaje_usuario\n";
+    $para = "nobody281812@gmail.com";
+    $asunto = "Nuevo mensaje de contacto: $nombre";
 
-$headers = "From: yisuskirisuto1205@gmail.com\r\n";
-$headers .= "Reply-To: $email\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $mensaje = "Has recibido un nuevo mensaje desde incolpartes.com\n\n";
+    $mensaje .= "Nombre: $nombre\n";
+    $mensaje .= "Email del remitente: $email\n";
+    $mensaje .= "Mensaje:\n$mensaje_usuario\n";
 
-mail($para, $asunto, $mensaje, $headers);
 
-header("Location: exito.html");
-exit();
+    $remitente_oficial = "contact@incolpartes.com"; 
+    
+    $headers = "From: $remitente_oficial\r\n";
+    $headers .= "Reply-To: $email\r\n"; 
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
 
+    $enviado = mail($para, $asunto, $mensaje, $headers, "-f" . $remitente_oficial);
+
+    if ($enviado) {
+        header("Location: exito.html");
+        exit();
+    } else {
+        echo "Error al enviar el mensaje. Verifica que la cuenta $remitente_oficial exista en IONOS.";
+    }
 }
 ?>
